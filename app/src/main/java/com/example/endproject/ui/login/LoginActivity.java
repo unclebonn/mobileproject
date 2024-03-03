cbb31a438f68d4655e17a71ff7238dfd1816ae95
unclebonn <khoita1008@gmail.com>
2024-03-03 17:47:06 +0700

    update fragment

---
+++ app/src/main/java/com/example/endproject/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.endproject.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.endproject.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
