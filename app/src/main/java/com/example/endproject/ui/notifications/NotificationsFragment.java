373f3dee3bb334960d4d725f0f84dfc57468bfb3
Minh Tam <tamnhmse161193@fpt.edu.vn>
2024-03-08 09:06:14 +0700

    logout still error

--- app/src/main/java/com/example/endproject/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/endproject/ui/notifications/NotificationsFragment.java
@@ -1,10 +1,18 @@
 package com.example.endproject.ui.notifications;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.endproject.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,6 +23,10 @@ import com.example.endproject.databinding.FragmentNotificationsBinding;
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,6 +41,31 @@ public class NotificationsFragment extends Fragment {
         return root;
     }
 
+    public void onCreate(Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        button = binding.logout;
+        textView = binding.textNotifications;
+        if (user == null){
+            Intent intent = new Intent(requireActivity(), LoginActivity.class);
+            requireActivity().startActivity(intent);
+            requireActivity().finish();
+        }
+        else {
+            String uid = user.getUid();
+            textView.setText(user.getEmail() + "\nUID: " + uid);
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(requireActivity(), LoginActivity.class);
+                requireActivity().startActivity(intent);
+                requireActivity().finish();
+            }
+        });
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
--- app/src/main/java/com/example/endproject/ui/register/RegisterActivity.java
+++ app/src/main/java/com/example/endproject/ui/register/RegisterActivity.java
@@ -1,16 +1,113 @@
 package com.example.endproject.ui.register;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import com.example.endproject.R;
 
+import androidx.annotation.NonNull;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.endproject.ui.login.LoginActivity;
+import com.example.endproject.ui.notifications.NotificationsFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    TextInputEditText editTextEmail, editTextPassword, editTextConfirmPassword;
+    Button buttonReg;
+    FirebaseAuth mAuth;
+    TextView textView;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), NotificationsFragment.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        editTextConfirmPassword = findViewById(R.id.confirm_password);
+        buttonReg = findViewById(R.id.btn_register);
+        textView = findViewById(R.id.loginNow);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password, confirmPassword;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+                confirmPassword = String.valueOf(editTextConfirmPassword.getText());
+
+                // Check if all fields are filled
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(RegisterActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(RegisterActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(confirmPassword)) {
+                    Toast.makeText(RegisterActivity.this, "Enter confirm password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Check if passwords match
+                if (!password.equals(confirmPassword)) {
+                    Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+                                    // open thẳng main activity
+                                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 }
