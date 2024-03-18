89d62b74f43c5e76a7f632c671b4a8bb7a42ec56
VINHZZZ <vinhdangquang1301@gmail.com>
2024-03-18 22:51:20 +0700

    viewShop_dashboard
    Viewshop

--- app/src/main/java/com/example/endproject/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/endproject/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.endproject.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,23 +15,42 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.endproject.R;
 import com.example.endproject.databinding.FragmentDashboardBinding;
+import com.example.endproject.ui.detail.DetailActivity;
+import com.example.endproject.ui.home.HomeFragment;
+import com.example.endproject.ui.login.LoginActivity;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
-   ImageView viewBack;
+   ImageView viewBack, viewShop;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         DashboardViewModel dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        //anh xa
         viewBack = root.findViewById(R.id.pharmImage);
+        viewShop = root.findViewById(R.id.clothingImage);
         viewBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle back press action
-                getActivity().onBackPressed();
+                Intent intent = new Intent(requireContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        viewShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new instance of the HomeFragment
+                HomeFragment homeFragment = new HomeFragment();
+
+                // Replace the current fragment with the HomeFragment
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.constraintLayout, homeFragment)
+                        .addToBackStack(null) // Optional: adds the transaction to the back stack
+                        .commit();
             }
         });
 
