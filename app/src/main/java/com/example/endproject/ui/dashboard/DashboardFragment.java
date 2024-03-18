bb8e0c2f2814a21b20d5867a48a227d804fe0254
VINHZZZ <vinhdangquang1301@gmail.com>
2024-03-18 22:23:12 +0700

    DashBoard_Back
    back

--- app/src/main/java/com/example/endproject/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/endproject/ui/dashboard/DashboardFragment.java
@@ -4,24 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.endproject.R;
 import com.example.endproject.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
-
+   ImageView viewBack;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         DashboardViewModel dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        viewBack = root.findViewById(R.id.pharmImage);
+        viewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle back press action
+                getActivity().onBackPressed();
+            }
+        });
+
 
 //        final TextView textView = binding.textDashboard;
 //        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
